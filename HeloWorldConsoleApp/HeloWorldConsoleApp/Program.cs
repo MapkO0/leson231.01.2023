ed95e60ab3e5eb85036492421486c2c8aac83d9f
Mapko0 <mekmek2@bk.ru>
2022-11-24 14:13:26 +0500

    HelloWorldFirstProject

---
+++ HeloWorldConsoleApp/HeloWorldConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeloWorldConsoleApp
+{
+	class Program
+	{
+		/// <summary>
+		/// Код, который запускается в первую очередь.
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Привет, Мир!");
+			Console.WriteLine("Привет");
+			Console.WriteLine("Мир");
+			Console.WriteLine("!!!");
+
+			Console.Write("HELLO ");
+			Console.Write("WORLD ");
+			Console.Write("!!!");
+
+			Console.ReadKey();
+		}
+
+	}
+}
