d569b1021c88c011fec8a09cc1fca312dd24e83a
Mark <118954790+MapkO0@users.noreply.github.com>
2023-03-10 20:39:50 +0500

    HomeWork 10.03.2023

---
+++ Leeson04-09.03.2023/Leeson04-09.03.2023/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeson04_09._03._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Создание матрицы \n Введите колличество строк \n");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов\n");
+            var b = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int[,] matrix = new int[a,b];
+
+            Random r = new Random();
+
+            
+            for (int i = 0; i < a; i++)
+            {
+                
+                for (int j = 0; j < b; j++)
+                {
+                    matrix[i, j] = r.Next(1, 10);
+                    
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                    Console.WriteLine();
+            }
+            foreach (int e in matrix)
+            {
+                sum += e;
+            }
+            Console.WriteLine("Cумма всех чисел в матрице = " +sum);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Leeson04-09.03.2023/Lesson04-exercise02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson04_exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Привет, давай создадим матрицы \n");
+            Console.WriteLine("Введите количество строк в матрице ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в матрице ");
+            int b = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int[,] matrixA = new int[a, b];
+            int[,] matrixB = new int[a, b];
+            Console.Write("Первая матрица: \n");
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    matrixA[i, j] = rnd.Next(0, 10);
+                    Console.Write($"\t{matrixA[i, j]}");
+                }
+                Console.WriteLine();
+                int[,] first = matrixA;
+            }
+            Console.WriteLine("\n");
+            Console.Write("Вторая матрица: \n");
+            for (int o = 0; o < a; o++)
+            {
+                for (int k = 0; k < b; k++)
+                {
+                    matrixB[o, k] = rnd.Next(0, 10);
+                    Console.Write($"\t{matrixB[o, k]}");
+                }
+                Console.WriteLine();
+                int[,] second = matrixB;
+            }
+            Console.WriteLine("\n");
+            Console.Write("Сумма двух матриц: \n");
+            int[,] matrixC = new int[a, b];
+            for (int p = 0; p < a; p++)
+            {
+                for (int l = 0; l < b; l++)
+                {
+                    matrixC[p, l] = matrixA[p, l] + matrixB[p, l];
+                    Console.Write($"\t{matrixC[p, l]}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
